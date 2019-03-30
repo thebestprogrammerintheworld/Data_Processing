a60c8630af7c4f7f3338a9a1771b073262ffc155
thebestprogrammerintheworld <47562831+thebestprogrammerintheworld@users.noreply.github.com>
2019-03-30 12:11:12 +0200

    Add files via upload

---
+++ v0.3/functions.h
@@ -0,0 +1,31 @@
+#ifndef FUNCTIONS_H_INCLUDED
+#define FUNCTIONS_H_INCLUDED
+
+#include <iostream>
+#include <stdlib.h>
+#include <iomanip>
+#include <conio.h>
+#include <windows.h>
+#include <time.h>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+class student_cls{
+    public:
+        string name, surname;
+        int mark_hw[50], mark_exam, mark_counter = 0, ID;
+        float calculate_hw(int option);
+        float calculate_final(float value);
+        float random();
+        void show_mark(float value);
+        void init();
+};
+void menu_navigation();
+void display_menu(int option);
+void display_results(int option);
+void display_intro(int option);
+void gotoxy(int x, int y);
+void ShowConsoleCursor(bool showFlag);
+
+#endif // FUNCTIONS_H_INCLUDED
